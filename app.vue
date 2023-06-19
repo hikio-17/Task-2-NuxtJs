59a987b737564b9ab6c51fe36636c2848066cff4
hikio-17 <fajri010217@gmail.com>
2023-06-19 08:44:53 -0700

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div class="flex items-stretch ...">
+      <div>01</div>
+      <div class="self-center">02</div>
+      <div>03</div>
+    </div>
+
+  </div>
+</template>
