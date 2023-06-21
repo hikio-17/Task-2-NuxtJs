b3ead44af38f8602e3da2cd03ceda199008eb08d
hikio-17 <fajri010217@gmail.com>
2023-06-21 08:24:34 -0700

    create chart

--- app.vue
+++ app.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    <div class="flex items-stretch ...">
-      <div>01</div>
-      <div class="self-center">02</div>
-      <div>03</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <Charts />
+      </div>
+      <div class="col-md-6 text-center">
+        Table Siswa
+      </div>
     </div>
-
   </div>
 </template>
+
+<script>
+import Charts from './components/Charts.vue'
+export default {
+  components: {
+    Charts
+  },
+  data() {
+    return {
+      name: "Hikma Nelda"
+    }
+  }
+}
+</script>
---
+++ components/Charts.vue
@@ -0,0 +1,43 @@
+<template>
+   <div class="w-100">
+      <Pie :data="chartData" :options="chartOptions" />
+   </div>
+</template>
+ 
+<script lang="ts">
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Pie } from 'vue-chartjs'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+export default {
+   components: {
+      Pie
+   },
+   data() {
+      return {
+         chartData: {
+            labels: [
+               'Red',
+               'Blue',
+               'Yellow'
+            ],
+            datasets: [
+               {
+                  backgroundColor: [
+                     'rgb(255, 99, 132)',
+                     'rgb(54, 162, 235)',
+                     'rgb(255, 205, 86)'
+                  ],
+                  data: [40, 20, 12]
+               }
+            ]
+         },
+         chartOptions: {
+            responsive: true
+         }
+      }
+   }
+}
+</script>
+ 
